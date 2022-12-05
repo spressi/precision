25781dc971d1e0ff2895a2b3468775b4d854c55a
Mario Reutter <30287556+spressi@users.noreply.github.com>
2022-12-05 23:40:24 +0100

    Add files via upload

---
+++ ECG Figure/ecg.R
@@ -0,0 +1,130 @@
+library(tidyverse)
+
+sampling.rate = 500
+threshold1 = .7
+threshold2 = .5
+
+myGgTheme = theme_bw() + theme(
+  plot.title = element_text(hjust = 0.5),
+  #axis.line = element_line(color='black'),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  panel.background = element_rect(fill="white", color="white"),
+  plot.background = element_rect(fill='white'), #element_blank(), #for transparency
+  legend.background = element_rect(fill="white", color=NA),
+  legend.key=element_rect(fill='white'),
+  axis.text = element_text(color="black"),
+  axis.ticks.x = element_line(color="black"),
+  axis.line.x = element_line(color="black"),
+  axis.line.y = element_line(color="black"),
+  legend.text = element_text(size=14, color="black"),
+  legend.title = element_text(size=14, color="black"),
+  strip.text.x = element_text(size=12, color="black"),
+  axis.text.x = element_text(size=16, color="black"),
+  axis.text.y = element_text(size=16, color="black"),
+  axis.title = element_text(size=16, color="black"))
+
+myGgTheme.pip = myGgTheme + theme(axis.text.x = element_text(size=10, color="black"),
+                                  axis.text.y = element_text(size=10, color="black"),
+                                  axis.title = element_text(size=10, color="black"),
+                                  legend.position="none")
+
+
+# Read and prepare data ---------------------------------------------------
+ecg = read.table("ecg.txt", sep="\t", col.names=c("min", "EDA", "ECG", "Trigger", "empty"), na.strings="", skip=15) %>% 
+  select(ECG) %>% tibble() %>% 
+  mutate(sample = 1:n(), time = (sample-1)/500) %>% 
+  filter(time > 1820, time < 1850) %>% mutate(time = time - min(time))
+  #filter(time > 1820, time < 1850+10) %>% mutate(time = time - min(time))
+
+# check data
+# ecg %>% ggplot(aes(x=time, y=ECG)) + 
+#   geom_hline(yintercept = threshold1, linetype="dashed", color="blue") +
+#   geom_hline(yintercept = threshold2, linetype="dashed", color="orange") +
+#   geom_line() + 
+#   ylab("ECG (mV)") + xlab("Time (sec)") + myGgTheme
+
+
+# Primitive Peak Detection ------------------------------------------------
+# this is just for a quick illustration and should not be used for regular R peak detection
+peakHelper = function(df, st, en) {
+  peak.index = df %>% filter(index >= st, index < en) %>% filter(ECG==max(ECG)) %>% pull(index) %>% first()
+  return(df %>% filter(index==peak.index) %>% pull(time))
+}
+ecg.peaks = ecg %>% mutate(peak = ECG > threshold2) %>% filter(peak) %>% select(-peak) %>% mutate(index = 1:n())
+peak.pos = ecg.peaks %>% pull(sample) %>% diff() %>% {. > 1} %>% which()
+peaks = tibble(peak.start = c(0, peak.pos), peak.end = c(peak.pos+1, ecg.peaks %>% nrow())) %>% 
+  rowwise() %>% mutate(peak.time = peakHelper(ecg.peaks, peak.start, peak.end)) %>% ungroup()
+peaks = peaks[setdiff(1:nrow(peaks), 25),] #manually delete beat 25 (counted twice due to ectopic beat)
+peaks = peaks %>% mutate(peak.time2 = peak.time); peaks[24, "peak.time2"] = peaks[c(23, 25), "peak.time2"] %>% colMeans()
+
+#shorten the plot
+ecg = ecg %>% filter(time > 10) %>% mutate(time = time - min(time)); peaks = peaks %>% filter(peak.time > 10, peak.time2 > 10) %>% mutate(peak.time = peak.time - 10, peak.time2 = peak.time2 - 10)
+#ecg = ecg %>% filter(time > 10, time < 20) %>% mutate(time = time - min(time)); peaks = peaks %>% filter(peak.time > 10, peak.time < 20, peak.time2 > 10, peak.time2 < 20) %>% mutate(peak.time = peak.time - 10, peak.time2 = peak.time2 - 10)
+
+peakDiff = peaks %>% filter(peak.time != peak.time2)
+original = peakDiff$peak.time
+interpolated = peakDiff$peak.time2
+
+#plot of raw ECG signal
+ecg.plot = ecg %>% ggplot(aes(x=time, y=ECG)) + 
+  # geom_hline(yintercept = threshold1, linetype="dashed", color="blue") +
+  # geom_hline(yintercept = threshold2, linetype="dashed", color="orange") +
+  geom_vline(xintercept = peaks$peak.time, linetype="dashed", color="#F8766D") +
+  geom_vline(xintercept = original, linetype="dashed", color="#F8766D") +
+  geom_vline(xintercept = interpolated, linetype="dashed", color="#00BFC4") +
+  geom_line() + 
+  scale_x_continuous(expand = c(0, 0)) +
+  ylab("ECG (mV)") + xlab("Time (sec)") + myGgTheme
+
+
+
+# calculate IBIs & HRV ----------------------------------------------------
+ibi = peaks %>% select(contains("peak.time")) %>% 
+  mutate(original = peak.time %>% diff() %>%  {. * 1000} %>% c(NA, .), #first ibi exists at the second R peak => prepend NA
+         interpol = peak.time2 %>% diff() %>%  {. * 1000} %>% c(NA, .),
+         intervall = 1:n()) %>% 
+  pivot_longer(c("original", "interpol"), values_to="ibi") %>% mutate(name = name %>% factor(levels=c("original", "interpol"))) %>% 
+  mutate(peak.time = ifelse(name=="original", peak.time, peak.time2)) %>% select(-peak.time2)
+
+ibi.plot.count = ibi %>% ggplot(aes(x=intervall, y=ibi, color=name)) +
+  geom_line(size=2) +
+  scale_x_continuous(expand = c(0, 0)) +
+  ylab("IBI (ms)") + xlab("Intervall (#)") + myGgTheme
+
+ibi.plot.time = ibi %>% ggplot(aes(x=peak.time, y=ibi, color=name)) + 
+  geom_line(size=2) + 
+  scale_x_continuous(expand = c(0, 0)) +
+  ylab("IBI (ms)") + xlab("Time (sec)") + myGgTheme
+
+
+hrv = ibi %>% group_by(name) %>% summarise(sdnn = ibi %>% sd(na.rm=T),
+                                           avnn = ibi %>% mean(na.rm=T),
+                                           rmssd = ibi %>% diff() %>% {. ^ 2} %>% mean(na.rm=T) %>% sqrt())
+hrv
+
+hrv.plot = hrv %>% ggplot(aes(x=name, y=rmssd, fill=name)) + geom_col(width=.75) + 
+  ylab("RMSSD (ms)") + xlab("") + myGgTheme + theme(legend.position = "none") +
+  scale_y_continuous(breaks=hrv$rmssd %>% round())
+
+inlay = cowplot::ggdraw(ibi.plot.time + theme(legend.position="none") + scale_y_continuous(limits = c(NA, 1000))) +
+  cowplot::draw_plot(hrv.plot + theme(plot.background = element_rect(fill='transparent', color=NA)), .5, .5, .5, .5) +
+  cowplot::draw_plot_label("C", x=.5)
+  
+
+{cowplot::plot_grid(ecg.plot, 
+                    ibi.plot.time + theme(legend.position="none") + scale_y_continuous(limits = c(NA, 1000)), 
+                    ncol = 1, labels="AUTO", align="hv", axis="l") %>% 
+    cowplot::ggdraw() + cowplot::draw_plot(hrv.plot + theme(plot.background = element_rect(fill='transparent', color=NA)), 
+                                           x=.55, width=.45, y=.25, height=.2) +
+    cowplot::draw_plot_label("C", x=.55, y=.5, hjust=1)} %>% 
+  ggsave("ECG.png", plot=., device="png", width=1920/300, height=2*1080/300, dpi=300)
+
+
+# cowplot::plot_grid(ecg.plot, 
+#                    ibi.plot.time + theme(legend.position="none") + scale_y_continuous(limits = c(NA, 1000)), 
+#                    #cowplot::plot_grid(hrv.plot+myGgTheme.pip, ggplot(), nrow=1, rel_widths = c(.5, .5)),
+#                    cowplot::plot_grid(hrv.plot, ggplot(), nrow=1, rel_widths = c(.5, .5)),
+#                    rel_heights = c(1, 1, .6),
+#                    ncol = 1, labels="AUTO", align="hv", axis="l") %>% 
+# ggsave("ECG.png", plot=., device="png", width=1920/300, height=1080/300*2.6, dpi=300)
---
+++ EDA Habituation/Habituation.R
@@ -0,0 +1,141 @@
+library(tidyverse)
+
+sampling.rate = 500
+trials = 160
+pause = 48.5
+
+eda = read.table("eda.txt", sep="\t", col.names=c("EDA", "ECG", "Trigger", "empty"), na.strings="", skip=15) %>% 
+  select(EDA, Trigger) %>% tibble() %>% 
+  mutate(sample = 1:n(),
+         time = (sample - 1) / 500,
+         trial = NA) #filled later
+
+trialOnsets = eda %>% filter({Trigger %>% diff() > 0} %>% c(F, .)) %>% pull(sample) %>% tail(trials)
+US_trials = read_tsv("ratings.csv") %>% filter(shock) %>% pull(trial)
+
+eda = eda %>% mutate(Trigger = case_when(sample %in% trialOnsets ~ 1,
+                                         sample %in% (trialOnsets[US_trials] + 5.85 * sampling.rate) ~ 2, #US application
+                                         T ~ 0))
+eda$trial[eda$Trigger == 1] = 1:trials; eda$trial[1] = 0; eda = eda %>% fill(trial, .direction="down")
+eda = eda %>% group_by(trial) %>% mutate(trialTime = time - min(time)) %>% ungroup() %>% 
+  mutate(kind = case_when(trial == 0 | (trialTime > 11) ~ "pause",
+                          (trial %in% US_trials) & (trialTime >= 5.85) ~ "shock", #TODO something about this doesn't work
+                          (trial-1 %in% US_trials) & (trialTime <= 5) ~ "shock", #let shocks overlap into onset of new stimuli
+                          T ~ "noshock") %>% as_factor(),
+         kind2 = ifelse(kind == "pause", "pause", "experiment") %>% as_factor())
+#eda %>% filter(kind == "pause") %>% filter(trialTime > 11) %>% pull(trial) %>% unique()
+
+
+eda.plot = eda %>% filter(#sample > min(trialOnsets) - 2 * sampling.rate, #seconds before first trial #not needed because US appears 5.85 seconds after trial onset
+                          trial != 0,
+                          sample < max(trialOnsets) + 11 * sampling.rate) %>% #seconds after last trial
+  mutate(sample = sample - min(sample), time = time - min(time)) #set beginning of sample & time to 0
+pause.us.time = eda.plot %>% filter(kind2 == "pause") %>% pull(time) %>% range() %>% mean()
+eda.plot = eda.plot %>% mutate(phase = case_when(kind2 == "pause" ~ "pause",
+                                                 time < pause.us.time ~ "first",
+                                                 T ~ "second"))
+US_start = eda.plot %>% filter(Trigger == 2) %>% mutate(trial.us = 1:n()) %>% select(trial.us, sample, time, trialTime)
+eda.us.plot = eda.plot %>% full_join(US_start %>% select(trial.us, sample), by="sample") %>% 
+  fill(trial.us, .direction="down") %>% filter(trial.us %>% is.na() == F) %>% 
+  group_by(trial.us) %>% mutate(trialTime.us = time - min(time)) %>% ungroup() %>% 
+  filter(trialTime.us <= 7)
+eda.us.scl = eda.us.plot %>% filter(trialTime.us < 1) %>% 
+  group_by(trial.us) %>% summarise(SCL = mean(EDA))
+eda.us.bl = eda.us.plot %>% full_join(eda.us.scl, by="trial.us") %>% mutate(EDA.bl = EDA - SCL)
+eda.us.scr = eda.us.bl %>% group_by(trial.us) %>% summarise(SCR = max(EDA.bl)) %>% full_join(eda.us.scl, by="trial.us") %>% 
+  left_join(eda.us.bl %>% select(trial, trial.us) %>% unique() %>% group_by(trial.us) %>% filter(trial == min(trial)), by="trial.us")
+pause.us = eda.us.scr %>% filter(trial < pause) %>% pull(trial.us) %>% max() %>% {. + .5}
+eda.us.scr = eda.us.scr %>% mutate(phase = ifelse(trial.us < pause.us, "first", "second") %>% as_factor())
+
+
+# Plots -------------------------------------------------------------------
+myGgTheme = theme_bw() + theme(
+  plot.title = element_text(hjust = 0.5),
+  #axis.line = element_line(color='black'),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  panel.border = element_blank(),
+  panel.background = element_rect(fill="white", color="white"),
+  plot.background = element_rect(fill='white'), #element_blank(), #for transparency
+  legend.background = element_rect(fill="white", color=NA),
+  legend.key=element_rect(fill='white'),
+  axis.text = element_text(color="black"),
+  axis.ticks.x = element_line(color="black"),
+  axis.line.x = element_line(color="black"),
+  axis.line.y = element_line(color="black"),
+  legend.text = element_text(size=14, color="black"),
+  legend.title = element_text(size=14, color="black"),
+  strip.text.x = element_text(size=12, color="black"),
+  axis.text.x = element_text(size=16, color="black"),
+  axis.text.y = element_text(size=16, color="black"),
+  axis.title = element_text(size=16, color="black"))
+
+#Panel A, Option 1: conformity to other panels (incorrect: we see all trials here, not only US trials => colors are different anyway)
+# eda.plot %>% 
+#   ggplot(aes(x = time/60, y = EDA, color=trial)) +
+#   geom_path() + myGgTheme + scale_color_viridis_c(trans = "reverse") +
+#   ylab("EDA (µS)") + xlab("Experiment Time (m)")
+
+#Panel A, Option 2: show trial kind (shock, noshock, pause)
+# eda.plot %>% ggplot(aes(x = time/60, y = EDA, color=kind, group=NA)) +
+#   geom_path() + myGgTheme + #theme(legend.position = "none") +
+#   scale_color_manual(values=c("shock" = "red", "noshock" = "blue", "pause" = "black")) +
+#   ylab("EDA (µS)") + xlab("Experiment Time (m)")
+
+#Panel A, Option 3: show trial kind2 (experiment pause)
+#eda.experiment = eda.plot %>% ggplot(aes(x = time/60, y = EDA, color=kind2, group=NA)) +
+eda.experiment = eda.plot %>% filter(kind2 != "pause") %>% ggplot(aes(x = time/60, y = EDA, group=phase)) +
+  geom_vline(xintercept=pause.us.time/60, color="grey") +
+  geom_vline(xintercept=eda.plot %>% filter(trial %in% US_trials) %>% group_by(trial) %>% 
+               summarise(USonset = min(time) + 5.85) %>% pull(USonset) %>% {. / 60}, 
+             color="red", linetype="dashed") +
+  geom_path() + myGgTheme + #theme(legend.position = "none") +
+  scale_color_manual("Phase", values=c("experiment" = "black", "pause" = "grey")) +
+  ylab("EDA (µS)") + xlab("Experiment Time (min)")
+eda.experiment
+
+
+#Panel C: UCRs
+eda.ucr = eda.us.plot %>% ggplot(aes(x = trialTime.us, y = EDA, color=trial.us, group=trial.us)) +
+  geom_vline(xintercept=0, color="red", linetype="dashed") +
+  geom_path() + scale_color_viridis_c(trans = "reverse") + myGgTheme + 
+  ylab("EDA (µS)") + xlab("Trial Time (sec)") + labs(color = "Trial")
+eda.ucr
+
+#Baseline-corrected UCR
+eda.ucr.bl = eda.us.bl %>% ggplot(aes(x = trialTime.us, y = EDA.bl, color=trial.us, group=trial.us)) +
+  geom_path() + scale_color_viridis_c(trans = "reverse") + myGgTheme +
+  ylab("BL-corrected EDA (µS)") + xlab("Experiment Time (sec)") + labs(color = "Trial")
+eda.ucr
+
+#Panel B: SCL
+#eda.scl = eda.us.scr %>% ggplot(aes(x = trial.us, y = SCL, color=trial.us)) +
+eda.scl = eda.us.scr %>% ggplot(aes(x = trial.us, y = SCL, color=trial.us, group=phase)) +
+  geom_vline(xintercept=pause.us, color="grey") + #break
+  geom_smooth(method="lm", color="black") + geom_point() + scale_color_viridis_c(trans = "reverse") + myGgTheme +
+  ylab("SCL (µS)") + xlab("Trial") + theme(legend.position = "none") #+ labs(color = "Trial")
+eda.scl
+
+#Panel D: SCR over trials
+eda.scr = eda.us.scr %>% ggplot(aes(x = trial.us, y = SCR, color=trial.us, group=phase)) +
+  geom_vline(xintercept=pause.us, color="grey") + #break
+  #facet_wrap(vars(phase), scales="free_x") +
+  geom_smooth(method="lm", color="black") + geom_point() + scale_color_viridis_c(trans = "reverse") + myGgTheme +
+  ylab("SCR (µS)") + xlab("Trial") + theme(legend.position = "none") #+ labs(color = "Trial")
+eda.scr
+
+#SCR x SCL
+eda.us.scr %>% ggplot(aes(x = SCR, y = SCL, color=phase, group=phase)) +
+  geom_smooth(method="lm") + geom_point() + myGgTheme
+
+
+# Export ------------------------------------------------------------------
+ggsave("EDA 1 experiment.png", plot=eda.experiment, device="png", width=1920/300, height=1080/300, dpi=300)
+ggsave("EDA 2 UCR.png", plot=eda.ucr, device="png", width=1920/300, height=1080/300, dpi=300)
+ggsave("EDA 3 UCR BL.png", plot=eda.ucr.bl, device="png", width=1920/300, height=1080/300, dpi=300)
+ggsave("EDA 4 SCL.png", plot=eda.scl, device="png", width=1920/300, height=1080/300, dpi=300)
+ggsave("EDA 5 SCR.png", plot=eda.scr, device="png", width=1920/300, height=1080/300, dpi=300)
+
+cowplot::plot_grid(eda.experiment, eda.scl,
+                   eda.ucr, eda.scr,
+                   ncol = 2, labels="AUTO", align="hv", axis="l") %>% 
+  ggsave("EDA.png", plot=., device="png", width=1920/300*2, height=1080/300*2, dpi=300)
